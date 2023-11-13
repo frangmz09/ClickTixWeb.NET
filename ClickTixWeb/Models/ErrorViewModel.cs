dfa53354dfeee16bb0aaffe81d30b70e1d189afc
JuampiLampu <144070275+JuampiLampu@users.noreply.github.com>
2023-11-13 19:26:34 -0300

    Agregar archivos de proyecto.

---
+++ ClickTixWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClickTixWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
