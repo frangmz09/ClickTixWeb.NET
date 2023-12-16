9e49cef5072bec0847d6cac66dafcdee0efd80d4
fran <frangomez0409@gmail.com>
2023-12-16 18:24:17 -0300

    falta uid en alta de ticket

--- ClickTixWeb/Controllers/CompraController.cs
+++ ClickTixWeb/Controllers/CompraController.cs
@@ -1,10 +1,88 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ClickTixWeb.Models;
+using FirebaseAdmin.Auth;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClickTixWeb.Controllers
 {
     public class CompraController : Controller
     {
+        private readonly ClicktixContext _context;
+        private readonly ILogger<DetalleController> _logger;
+        FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+
+
+        public CompraController(ILogger<DetalleController> logger, ClicktixContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        [HttpPost]
+        public ActionResult ConfirmarCompra(IFormCollection formCollection)
+        {
+            var funcionId = int.Parse(formCollection["funcionId"]);
+            var precioMomento = decimal.Parse(formCollection["precioMomento"]);
+            var email = formCollection["emailHidden"];
+            var asientosIds = formCollection["asientosId"].Select(id => int.Parse(id)).ToList();
+            string UserId = ObtenerUid(email);
+
+            foreach (var asientoId in asientosIds)
+            {
+                var asiento = _context.Asientos.SingleOrDefault(a => a.Id == asientoId);
+
+                if (asiento != null)
+                {
+                    asiento.Disponible = 0;
+                }
+            }
+
+            _context.SaveChanges();
+            foreach (var idAsiento in asientosIds)
+            {
+                DateTime fechaActual = DateTime.Now;
+                var nuevoTicket = new Ticket
+                {
+                    IdFuncion = funcionId,
+                    Fecha = fechaActual,
+                    Fila = ObtenerFilaPorId(idAsiento), 
+                    Columna = ObtenerColumnaPorId(idAsiento), 
+                    PrecioAlMomento = (double)precioMomento,
+                    IdUsuario = UserId,
+                    };
+                    _context.Tickets.Add(nuevoTicket);
+
+                }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home"); 
+        }
+        public int ObtenerFilaPorId(int asientoId)
+        {
+            var fila = _context.Asientos
+                .Where(asiento => asiento.Id == asientoId)
+                .Select(asiento => asiento.Fila)
+                .FirstOrDefault();
+
+            return (int)fila;
+        }
+        public int ObtenerColumnaPorId(int asientoId)
+        {
+            var fila = _context.Asientos
+                .Where(asiento => asiento.Id == asientoId)
+                .Select(asiento => asiento.Columna)
+                .FirstOrDefault();
+
+            return (int)fila;
+        }
+        public string ObtenerUid(string email)
+        {
+            var userRecord = auth.GetUserByEmailAsync(email).Result;
+
+            return userRecord.Uid;
+
+        }
+
         // GET: CompraController
         public ActionResult Index()
         {
